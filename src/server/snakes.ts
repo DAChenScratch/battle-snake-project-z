b0be55fdb984e661555e945d03933760ea5adafd
David Neilsen <petah.p@gmail.com>
2019-12-11 00:05:12 +1300

    Enable snakes

--- src/server/snakes.ts
+++ src/server/snakes.ts
@@ -12,11 +12,11 @@ import { ProjectZ2 } from "./snakes/project-z-2";
 export default {
     9001: ProjectZ,
     9002: KeepAway,
-    // 9003: Rando,
+    9003: Rando,
     9004: Tak,
     9005: TailChase,
-    // 9006: Aldo,
-    // 9007: Dunno,
-    // 9008: WorkItOut,
+    9006: Aldo,
+    9007: Dunno,
+    9008: WorkItOut,
     9009: ProjectZ2,
 };
