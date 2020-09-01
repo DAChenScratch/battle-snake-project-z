77c5ef234c5268170c68edb44e1fd9d90d69ba33
David Neilsen <petah.p@gmail.com>
2020-09-01 21:29:59 +1200

    Fix aldo

--- src/server/snakes/aldo.ts
+++ src/server/snakes/aldo.ts
@@ -6,12 +6,19 @@ import { Tak } from './tak';
 import { TailChase } from './tail-chase';
 import { BaseSnake } from './base-snake';
 import { ISnake } from './snake-interface';
+import { Color } from '../../types/Color';
+import { HeadType } from '../../types/HeadType';
+import { TailType } from '../../types/TailType';
 
 const CRC32 = require('crc-32');
 
 export class Aldo extends BaseSnake implements ISnake {
     public port: number = 9006;
     private options: any[];
+
+    public color = Color.POMEGRANATE;
+    public headType = HeadType.TONGUE;
+    public tailType = TailType.FRECKLED;
 
     constructor() {
         super();
